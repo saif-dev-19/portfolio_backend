6da07fe01500993219ad288acb55c3b15584dc14
saif-dev-19 <anonymous.safu@gmail.com>
2026-06-10 13:21:22 +0600

    fix error

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+import time
+from django.db import connections
 
 
 class PortfolioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "portfolio"
+
+
+def wait_for_db():
+    for i in range(10):
+        try:
+            connections["default"].cursor()
+            return
+        except Exception:
+            time.sleep(2)
